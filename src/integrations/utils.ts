15d32b6911b1dc4ab088b3285a2d90ec58844e46
Alex Bespoyasov <aleksandrb@spotify.com>
2026-07-24 08:48:17 +0200

    chore: handle missing CLIs

--- src/integrations/utils.ts
+++ src/integrations/utils.ts
@@ -3,3 +3,10 @@ import { STATUS_PREFIX } from './constants.js';
 export function normalizeStatusLine(line: string) {
   return line.startsWith(STATUS_PREFIX) ? line.slice(STATUS_PREFIX.length) : line;
 }
+
+export function spawnErrorMessage(bin: string, err: NodeJS.ErrnoException): string {
+  if (err.code === 'ENOEXEC' || err.code === 'ENOENT') {
+    return `${bin} CLI not found or not executable. Make sure it is installed and on your PATH.`;
+  }
+  return err.message;
+}
